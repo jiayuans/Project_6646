b2cda329f08a75197a01d727b92c03f291e8a159
Jiayuan Shi <jiayuans422@gmail.com>
2024-11-27 23:57:09 -0700

    6646 it

--- out.rec_it.R
+++ out.rec_it.R
@@ -1,12 +1,12 @@
-setwd("C:/Users/jiayu/OneDrive/Desktop/BIOS6646")
+setwd("C:/Users/jiayu/OneDrive/Desktop/BIOS6646_it")
 
 ###########################################################################
 # Read csv files
-text <- list.files(pattern="rec.result.")
+text <- list.files(pattern="rec.result_it.")
 num <- as.numeric(unlist(lapply(strsplit(text,'.',fixed=TRUE),function(x) x[[3]])))
 
 data_frames <- lapply(num, function(i) {
-  file_name <- paste0("rec.result.", i, ".csv") 
+  file_name <- paste0("rec.result_it.", i, ".csv") 
   read.csv(file_name)
 })
 
